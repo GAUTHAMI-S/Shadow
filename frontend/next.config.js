ea22227ec447585494002457a0c9b3eac5b48e34
Gauthami S <gauthami.s@gadgeon.com>
2026-03-28 20:18:26 +0530

    fixed issue in pwa

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,32 +1,8 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('@ducanh2912/next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
-      handler: 'CacheFirst',
-      options: { cacheName: 'google-fonts', expiration: { maxEntries: 4, maxAgeSeconds: 365 * 24 * 60 * 60 } },
-    },
-    {
-      urlPattern: /\/api\/habits$/,
-      handler: 'StaleWhileRevalidate',
-      options: { cacheName: 'api-habits', expiration: { maxEntries: 32, maxAgeSeconds: 24 * 60 * 60 } },
-    },
-    {
-      urlPattern: /\/api\/.*/,
-      handler: 'NetworkFirst',
-      options: { cacheName: 'api-cache', networkTimeoutSeconds: 10 },
-    },
-  ],
-});
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: { domains: [] },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
